6c0a5f16c0547e70d1470a1fb456332bfc03bea2
hel <my@maggit.ru>
2018-10-21 11:53:35 +0300

    Add gh-pages

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # swapi
+https://ponchicken.github.io/swapi-test/
 
 ## Project setup
 ```
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production'
+  ? '/swapi-test/'
+  : '/',
   css: {
     sourceMap: true
   },
